29325ffa24463268b792454612ae9f356c0aa342
PranavKumar27 <pranav.jenus@gmail.com>
2021-09-23 19:36:44 +0530

    Update _4_Qck_Sel_Find_kth_elment.cpp

--- _4_Qck_Sel_Find_kth_elment.cpp
+++ _4_Qck_Sel_Find_kth_elment.cpp
@@ -1,1 +1,103 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+//Find the K-th smallest element
+// from the given Array
+
+static int ans = -1;
+void print_v(vector<int> v);
+int partition(vector<int> &v, int l, int r)
+{
+	int i, j;
+
+	int pivot = v[r];
+	i = l - 1;
+	j = l;
+	while (j < r)
+	{
+		if (v[j] > pivot)
+		{
+			// no problem
+			j++;
+		}
+		else if (v[j] < pivot)
+		{
+			i++;
+			swap(v[i], v[j]);
+			j++;
+		}
+	}
+
+	i++;
+	swap(v[i], v[r]);
+	//cout << "DBG " <<" l=" << l << "  r=" << r << " pivot=" << pivot << endl;
+	//print_v(v);
+	return i; // pivot correct postional index
+}
+
+void QuickSelect(vector<int> &v, int l, int r, int k)
+{
+	int pivot;
+
+	pivot = partition(v, l, r);
+
+	if (pivot == k - 1)
+	{
+		//cout << "DBG " << "Found kth value= " << v[pivot] << endl;
+		//cout << "DBG " << "Found index=" << pivot << endl;
+		ans = v[pivot];
+	}
+
+	if (l >= r)
+	{
+		return;
+	}
+
+	else if (pivot < k)
+	{
+		QuickSelect(v, pivot + 1, r, k);
+	}
+
+	else
+	{
+		QuickSelect(v, l, pivot - 1, k);
+	}
+}
+
+void print_v(vector<int> v)
+{
+	for (auto z : v)
+	{
+		cout << z << ",";
+	}
+	cout << endl;
+}
+int main(int argc, char *argv[])
+{
+	vector<int> v = {10, 5, 2, 0, 6, 4};
+	int k;
+
+	cout << "Enter K:";
+	cin >> k;
+
+	cout << "Vector v=" << endl;
+	print_v(v);
+	cout << "Sorted Vector=" << endl;
+	vector<int> v_cpy(v.begin(), v.end());
+	sort(v_cpy.begin(), v_cpy.end());
+	print_v(v_cpy);
+
+	if (k > v.size())
+	{
+		cout << "Plese Enter Valid K" << endl;
+	}
+	else
+	{
+		QuickSelect(v, 0, v.size() - 1, k);
+
+		cout << "Result=" << ans << endl;
+	}
+	return 0;
+}
